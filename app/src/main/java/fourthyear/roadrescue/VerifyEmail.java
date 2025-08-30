7d58c34b30557fa271cc3171cc83cea06d5df7ba
JRioP <riojoshuadev@gmail.com>
2025-08-30 11:05:52 +0800

    Email Verification Continuation

--- app/src/main/java/fourthyear/roadrescue/VerifyEmail.java
+++ app/src/main/java/fourthyear/roadrescue/VerifyEmail.java
@@ -1,6 +1,8 @@
 package fourthyear.roadrescue;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class VerifyEmail extends AppCompatActivity {
+
+    EditText digitNumberOne,digitNumberTwo,digitNumberThree,digitNumberFour,digitNumberFive,digitNumberSix;
+    Button verifyBtn, resendBtn;
+
+    Boolean digitValid;
+
+    FirebaseAuth fAuth;
+    EmailAuthCredential eAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
